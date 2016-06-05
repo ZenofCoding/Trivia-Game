bcd846fecea1ce74940b0c8fdf95ce2068699327
joe shin <joe.shin@live.com>
2016-06-04 23:42:11 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,153 @@
+
+var correct = 0;
+var wrong = 0;
+var answersRight = [];
+var answersWrong = [];
+var userClick = '';
+var questionNumber;
+
+
+var choice1 = [
+  {
+  choices:["Nosferatu", "Lumiere", "Citzen Kane", "Batman"],
+  answer: 1
+}];
+
+var choice2 = [
+{
+choices:["Basketball Diaries", "The Outsiders", "Inception", "The Road"],
+answer: 3
+}];
+
+var choice3 = [
+{
+choices: ["Jack Nicholson", "Al Pacino", "Robert DeNiro", "Christian Bale"],
+answer: 0
+}];
+
+var choice4 = [
+{
+choices: ["Waterworld", "The Matrix", "Batman", "Pirates of the Carribean"],
+answer: 3
+}];
+
+var choice5 = [
+{
+choices: ["Jaws", "Blade Runner", "Mrs.Doubtfire", "Tron"],
+answer: 3
+}];
+
+var choice6 = [
+{
+choices: ["Braveheart", "The Departed", "Momento", "Scarface"],
+answer: 2
+}];
+
+var choice7 = [
+{
+choices: ["Aladdin", "Anatasia", "Mulan", "Cinderella"],
+answer: 1
+}];
+
+var choice8 = [
+{
+choices: ["Heath Ledger", "Robbin Williams", "Brandon Lee", "James Dean"],
+answer: 2
+}];
+
+var choice9 = [
+{
+choices: ["Johnny Depp", "Tom Cruise", "Mel Gibson", "Jack Nicholson"],
+answer: 1
+}];
+
+var choice10 = [
+{
+choices: ["Iron Man", "Spider Man", "Batman", "Superman"],
+answer: 0
+}];
+
+$(document).ready(function() {
+  
+if($('input')==choice1[1]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+if($('input')==choice1[3]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+    
+if($('input')==choice1[0]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+if($('input')==choice1[3]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+if($('input')==choice1[3]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+if($('input')==choice1[2]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+if($('input')==choice1[1]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+if($('input')==choice1[2]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+if($('input')==choice1[1]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+if($('input')==choice1[0]) {
+    prompt("Correct!");
+}
+
+else {
+  prompt("Sorry");
+};
+
+} // syntax error.... 
+
